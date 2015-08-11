0ce0d9346a698f97873049c5bcf4d9910ce67179
MuellerDaniel <mueller.daniel89@gmx.de>
2015-08-11 15:57:04 +0200

    code for displaying/plotting data

--- python/displayDataContinous.py
+++ python/displayDataContinous.py
@@ -2,82 +2,117 @@ from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.ptime import time
-import serial
-import time
-import pyqtgraph.console
+import subprocess
+import dataAcquisitionMulti as datAcM
  
 app = QtGui.QApplication([])
  
 p = pg.plot()
-p.setWindowTitle('live plot from serial')
-curve1 = p.plot(pen=(255,0,0))
-curve2 = p.plot(pen=(0,255,0))
-curve3 = p.plot(pen=(0,0,255))
+p.setWindowTitle('live plot from ble')
+p.addLegend()
+curvesRed = []
+curvesRed.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.SolidLine), name='x'))
+curvesRed.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.DashLine), name='y'))
+curvesRed.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.DotLine), name='z'))
+curvesBlue = []
+curvesBlue.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(0,0,255), style=QtCore.Qt.SolidLine), name='x'))
+curvesBlue.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(0,0,255), style=QtCore.Qt.DashLine), name='y'))
+curvesBlue.append(p.plot(pen=pg.mkPen(color=QtGui.QColor(0,0,255), style=QtCore.Qt.DotLine), name='z'))
+#curve1 = p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.SolidLine), name='x')
+#curve2 = p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.DashLine), name='y')
+#curve3 = p.plot(pen=pg.mkPen(color=QtGui.QColor(255,0,0), style=QtCore.Qt.DotLine), name='z')
 
 datax = [0]
 datay = [0]
 dataz = [0]
-dataArr = np.empty(shape=[0,3])
-dataArr = np.append(dataArr, np.array([0,100,200]))
-#raw=serial.Serial("/dev/ttyACM0",9600)
-#raw.open()
-cntarr = np.array([0,100,200])
-cnt = 0
-#cnt[0] = 0
-#cnt[1] = 100
-#cnt[2] = 200
+dataArrRed = [[0.,0.,0.]]
+dataArrBlue = [[0.,0.,0.]]
 
+#dataArr = np.empty(300)
+displayArr = np.empty(shape=[0,3])
+#dataArr = np.append(dataArr, np.array([0,100,200]))
+
+cnt = 0
+overcnt = 0
+overcntRed = 0
+overcntBlue = 0
+maxSize = 100
 
 def update():
-    global curve1, curve2, curve3, datax, datay, dataz, cntarr, cnt, dataArr
-#    cntarr += np.array([1, -1, 0])
-#    cnt += 1
-#    dataArr = np.append(dataArr, cntarr)
-#    dataArr = np.reshape(dataArr, (dataArr.size/3, 3))
+    global cnt, dataArrRed, dataArrBlue, maxSize, overcnt, overcntRed, overcntBlue, curves
     
-    datax.append(cntarr[0])
-    datay.append(cntarr[1])
-    dataz.append(cntarr[2])
-    #data.append(cnt)
-    #line = raw.readline()
-    #data.append(int(line))
-#    xdata = np.array(datax)
-#    ydata = np.array(datay)
-#    zdata = np.array(dataz)    
-    #print "xdata ", xdata    
-    #print "ArrXdata ", dataArr[:,0]    
+    if b[0] == 0:
+        dataArrRed = np.append(dataArrRed, [b[1:]], axis=0)
+    elif b[0] == 1:
+        dataArrBlue = np.append(dataArrBlue, [b[1:]], axis=0)
+#    dataArr = np.reshape(dataArr, (dataArr.size/3,3))
     
-#    curve1.setData(xdata)
-#    curve2.setData(ydata)
-#    curve3.setData(zdata)
-    curve1.setData(dataArr[:,0])
-    curve2.setData(dataArr[:,1])
-    curve3.setData(dataArr[:,2])
+    cnt += 1
+    
+    if dataArrRed.shape[0]>maxSize:
+#        overcnt+=1
+#        curve1.setData(dataArr[overcnt:,0])
+#        curve2.setData(dataArr[overcnt:,1])
+#        curve3.setData(dataArr[overcnt:,2])
+        if b[0] == 0:
+            overcntRed += 1
+            curvesRed[0].setData(dataArrRed[overcnt:,0])
+            curvesRed[1].setData(dataArrRed[overcnt:,1])
+            curvesRed[2].setData(dataArrRed[overcnt:,2])
+        elif b[0] == 1:
+            overcntBlue += 1
+            curvesBlue[0].setData(dataArrBlue[overcnt:,0])
+            curvesBlue[1].setData(dataArrBlue[overcnt:,1])
+            curvesBlue[2].setData(dataArrBlue[overcnt:,2])    
+
+    else:
+#        curve1.setData(dataArr[:,0])
+#        curve2.setData(dataArr[:,1])
+#        curve3.setData(dataArr[:,2])
+        if b[0] == 0:
+            curvesRed[0].setData(dataArrRed[:,0])
+            curvesRed[1].setData(dataArrRed[:,1])
+            curvesRed[2].setData(dataArrRed[:,2])
+        elif b[0] == 1:
+            curvesBlue[0].setData(dataArrBlue[:,0])
+            curvesBlue[1].setData(dataArrBlue[:,1])
+            curvesBlue[2].setData(dataArrBlue[:,2])
+        
+    
+#    curve3.setPos(-cnt, 0)
     
     app.processEvents()
- 
-#timer = QtCore.QTimer()
-#timer.timeout.connect(update)
-#timer.start(100)
+    
+
+
+# BLE acquisition
+proc = subprocess.Popen("gatttool -t random -b E3:C0:07:76:53:70 --char-write-req --handle=0x000f --value=0300 --listen".split(), 
+                        stdout=subprocess.PIPE, close_fds=True)
+# serial acquisition
+#proc = subprocess.Popen("stty -F /dev/ttyUSB0 time 50; cat /dev/ttyUSB0", 
+#                        stdout=subprocess.PIPE, close_fds=True, shell=True) 
+b = np.array([0.,0.,0.,0.]) 
+i = 0
 try:
-    while True:
-        cntarr += np.array([1, -1, 0])
-        cnt += 1
-        dataArr = np.append(dataArr, cntarr)
-        dataArr = np.reshape(dataArr, (dataArr.size/3, 3))        
-  
+    while True:          
+        output = proc.stdout.readline()
+        b = datAcM.structDataBLE(output)   
+#        b = datAc.structDataSer(output)
         update()
-        #time.sleep(.5)
-        
-except KeyboardInterrupt:
-    print "out!"
-    print "dataArr", dataArr.size
+
+
+# to catch a ctrl-c
+except KeyboardInterrupt:    
+    proc.stdout.close()
+    proc.kill()
+    print "Killed ble listener!"
     pass
 
 
 #app.closeAllWindows()
     
-#if __name__ == '__main__':
-#    import sys
-#    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-#        QtGui.QApplication.instance().exec_()
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+        print "here!"
--- python/plotting.py
+++ python/plotting.py
@@ -5,57 +5,144 @@ Created on Wed Jul 15 10:00:35 2015
 @author: daniel
 """
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-def plotter(inputlist, titlelist, shareAxis = True):
+def plotter2d(inputlist, titlelist, shareAxis = True):
+    """plot the data
+    
+    Parameters
+    ----------
+    inputlist : list
+        list of the data matrices
+    titlelist : list
+        list for the corresponding titles
+    shareAxis : bool
+        share the y-axis or not
+            
+       
+    """        
+    print "len(inputlist) ", len(inputlist)
+    print "len(titlelist) ", len(titlelist)
+    if len(inputlist) != len(titlelist):
+        print "len(inputlist) != len(titlelist)"
+        pass
+    else:
+        plotIt2d(inputlist,titlelist, len(inputlist), shareAxis)
+    
+    
+def plotIt2d(data, title, sizeData, shareAxis):
+    initPos = 0
+        
+    if len(data[0][0]) == 4:
+        initPos = 1
+    
+    if sizeData == 1:
+#        for i in range(len(data[0])):
+        if max(data[0][:,0]) == 0:
+            plt.plot(data[0][:,initPos], color='b', ls='-', label='x')
+            plt.plot(data[0][:,initPos + 1], color='b', ls='--', label='y')
+            plt.plot(data[0][:,initPos + 2], color='b', ls=':', label='z')
+            plt.title(title[0])
+            plt.legend()
+        
+        if max(data[0][:,0]) == 1:
+            data0=[[0.,0.,0.]]
+            data1=[[0.,0.,0.]]
+            for i in range(len(data[0])):
+                tmp = data[0][i]                
+                if tmp[0] == 0:
+                    data0=np.append(data0, [data[0][i][1:]], axis=0)
+                elif tmp[0] == 1:
+                    data1=np.append(data1, [data[0][i][1:]], axis=0)
+                
+            plt.plot(data0[:,0], color='b', ls='-', label='x')
+            plt.plot(data0[:,1], color='b', ls='--', label='y')
+            plt.plot(data0[:,2], color='b', ls=':', label='z')
+            plt.plot(data1[:,0], color='r', ls='-', label='x')
+            plt.plot(data1[:,1], color='r', ls='--', label='y')
+            plt.plot(data1[:,2], color='r', ls=':', label='z')
+            
+            plt.title(title[0])
+            plt.legend()
+                
+#        plt.plot(data[0][:,initPos], color='b', ls='-', label='x')
+#        plt.plot(data[0][:,initPos + 1], color='b', ls='--', label='y')
+#        plt.plot(data[0][:,initPos + 2], color='b', ls=':', label='z')
+#        plt.title(title[0])
+#        plt.legend()
+        
+    elif sizeData == 2:
+        f,(one, two) = plt.subplots(1,2, sharey=shareAxis)
+        one.plot(data[0][:,initPos], color='b', ls='-', label='x')
+        one.plot(data[0][:,initPos + 1], color='b', ls='--', label='y')
+        one.plot(data[0][:,initPos + 2], color='b', ls=':', label='z')
+        one.set_title(title[0])
+        one.legend()
+        
+        two.plot(data[1][:,initPos], color='b', ls='-', label='x')
+        two.plot(data[1][:,initPos + 1], color='b', ls='--', label='y')
+        two.plot(data[1][:,initPos + 2], color='b', ls=':', label='z')
+        two.set_title(title[1])
+        
+    elif sizeData == 3:
+        f, (one, two, three) = plt.subplots(1,3, sharey=shareAxis)
+        one.plot(data[0][:,initPos], color='b', ls='-', label='x')
+        one.plot(data[0][:,initPos + 1], color='b', ls='--', label='y')
+        one.plot(data[0][:,initPos + 2], color='b', ls=':', label='z')
+        one.set_title(title[0])
+        one.legend()
+        
+        two.plot(data[1][:,initPos], color='b', ls='-', label='x')
+        two.plot(data[1][:,initPos + 1], color='b', ls='--', label='y')
+        two.plot(data[1][:,initPos + 2], color='b', ls=':', label='z')
+        two.set_title(title[1])
+    
+        three.plot(data[2][:,initPos], color='b', ls='-', label='x')
+        three.plot(data[2][:,initPos + 1], color='b', ls='--', label='y')
+        three.plot(data[2][:,initPos + 2], color='b', ls=':', label='z')
+        three.set_title(title[2])
+        
+def plotter3d(inputlist, titlelist):
+    """plot the data
+    
+    Parameters
+    ----------
+    inputlist : list
+        list of the data matrices
+    titlelist : list
+        list for the corresponding titles
+                
+       
+    """    
     print "len(inputlist) ", len(inputlist)
     print "len(titlelist) ", len(titlelist)
     if len(inputlist) != len(titlelist):
         print "not enough title given!"
         pass
     else:
-        plotIt(inputlist,titlelist, len(inputlist), shareAxis)
-    
-    
-def plotIt(data, title, sizeData, shareAxis):
+        plotIt3d(inputlist,titlelist, len(inputlist))
+        
+def plotIt3d(data, title, sizeData):
+    fig = plt.figure()
     initPos = 0
-    
     if len(data[0][0]) == 4:
         initPos = 1
     
     if sizeData == 1:
-        plt.plot(data[0][:,initPos], color='r', label='x')
-        plt.plot(data[0][:,initPos + 1], color='g', label='y')
-        plt.plot(data[0][:,initPos + 2], color='b', label='z')
-        plt.title(title[0])
-        plt.legend()
-        
+        ax = fig.gca(projection='3d')
+        ax.plot(data[0][:,initPos], data[0][:,initPos+1], data[0][:,initPos+2])
     elif sizeData == 2:
-        f,(one, two) = plt.subplots(1,2, sharey=shareAxis)
-        one.plot(data[0][:,initPos], color='r', label='x')
-        one.plot(data[0][:,initPos + 1], color='g', label='y')
-        one.plot(data[0][:,initPos + 2], color='b', label='z')
-        one.set_title(title[0])
-        one.legend()
-        
-        two.plot(data[1][:,initPos], color='r', label='x')
-        two.plot(data[1][:,initPos + 1], color='g', label='y')
-        two.plot(data[1][:,initPos + 2], color='b', label='z')
-        two.set_title(title[1])
-        
+        ax = fig.add_subplot(1,2,1, projection='3d', title=title[0])
+        ax.plot(data[0][:,initPos], data[0][:,initPos+1], data[0][:,initPos+2])
+        ax = fig.add_subplot(1,2,2, projection='3d', title=title[1])
+        ax.plot(data[1][:,initPos], data[1][:,initPos+1], data[1][:,initPos+2])
     elif sizeData == 3:
-        f, (one, two, three) = plt.subplots(1,3, sharey=shareAxis)
-        one.plot(data[0][:,initPos], color='r', label='x')
-        one.plot(data[0][:,initPos + 1], color='g', label='y')
-        one.plot(data[0][:,initPos + 2], color='b', label='z')
-        one.set_title(title[0])
-        one.legend()
+        ax = fig.add_subplot(1,3,1, projection='3d', title=title[0])
+        ax.plot(data[0][:,initPos], data[0][:,initPos+1], data[0][:,initPos+2])
+        ax = fig.add_subplot(1,3,2, projection='3d', title=title[1])
+        ax.plot(data[1][:,initPos], data[1][:,initPos+1], data[1][:,initPos+2])
+        ax = fig.add_subplot(1,3,3, projection='3d', title=title[2])
+        ax.plot(data[2][:,initPos], data[2][:,initPos+1], data[2][:,initPos+2])
         
-        two.plot(data[1][:,initPos], color='r', label='x')
-        two.plot(data[1][:,initPos + 1], color='g', label='y')
-        two.plot(data[1][:,initPos + 2], color='b', label='z')
-        two.set_title(title[1])
     
-        three.plot(data[2][:,initPos], color='r', label='x')
-        three.plot(data[2][:,initPos + 1], color='g', label='y')
-        three.plot(data[2][:,initPos + 2], color='b', label='z')
-        three.set_title(title[2])
