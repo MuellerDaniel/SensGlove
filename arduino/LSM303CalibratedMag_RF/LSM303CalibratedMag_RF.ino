53a8a71fd61a57c31af01039caa45c787a444fc7
MuellerDaniel <mueller.daniel89@gmx.de>
2015-09-25 08:47:03 +0200

    added explanation

--- arduino/LSM303CalibratedMag_RF/LSM303CalibratedMag_RF.ino
+++ arduino/LSM303CalibratedMag_RF/LSM303CalibratedMag_RF.ino
@@ -1,30 +1,16 @@
 /*
-The sensor outputs provided by the library are the raw 16-bit values
-obtained by concatenating the 8-bit high and low accelerometer and
-magnetometer data registers. They can be converted to units of g and
-gauss using the conversion factors specified in the datasheet for your
-particular device and full scale setting (gain).
-
-Example: An LSM303D gives a magnetometer X axis reading of 1982 with
-its default full scale setting of +/- 4 gauss. The M_GN specification
-in the LSM303D datasheet (page 10) states a conversion factor of 0.160
-mgauss/LSB (least significant bit) at this FS setting, so the raw
-reading of -1982 corresponds to 1982 * 0.160 = 317.1 mgauss =
-0.3171 gauss.
-
-In the LSM303DLHC, LSM303DLM, and LSM303DLH, the acceleration data
-registers actually contain a left-aligned 12-bit number, so the lowest
-4 bits are always 0, and the values should be shifted right by 4 bits
-(divided by 16) to be consistent with the conversion factors specified
-in the datasheets.
-
-Example: An LSM303DLH gives an accelerometer Z axis reading of -16144
-with its default full scale setting of +/- 2 g. Dropping the lowest 4
-bits gives a 12-bit raw value of -1009. The LA_So specification in the
-LSM303DLH datasheet (page 11) states a conversion factor of 1 mg/digit
-at this FS setting, so the value of -1009 corresponds to -1009 * 1 =
-1009 mg = 1.009 g.
-*/
+ * SIMPLE CALIBRATION OF MAGNETOMETER DATA
+ * 
+ * You just take a nr of measurements, waving your sensor around.
+ * Then you calculate the hard iron offset, by aligning the values around the center
+ * To calculate the soft iron offset (scale factor), 
+ * you take the average radius of the 3 directions, which is the average radius
+ * and calculate a scale factor for each axis, to scale the measurments to this radius
+ * 
+ * according to: 
+ * https://github.com/kriswiner/MPU-6050/wiki/Simple-and-Effective-Magnetometer-Calibration
+ * 
+ */
 
 #include <Wire.h>
 #include <LSM303.h>
@@ -39,7 +25,7 @@ float softBias[3];
 //datasheet p.10: resolution of 0.16 mgauss/LSB
 
 float conversionFactor = 0.479;   //for range +-12gauss
-int sampleNum = 1000;
+int sampleNum = 2000;
 
 void setup()
 {
--- arduino/MagData_LSM303_RF/MagData_LSM303_RF.ino
+++ arduino/MagData_LSM303_RF/MagData_LSM303_RF.ino
@@ -1,45 +1,27 @@
 /*
-The sensor outputs provided by the library are the raw 16-bit values
-obtained by concatenating the 8-bit high and low accelerometer and
-magnetometer data registers. They can be converted to units of g and
-gauss using the conversion factors specified in the datasheet for your
-particular device and full scale setting (gain).
-
-Example: An LSM303D gives a magnetometer X axis reading of 1982 with
-its default full scale setting of +/- 4 gauss. The M_GN specification
-in the LSM303D datasheet (page 10) states a conversion factor of 0.160
-mgauss/LSB (least significant bit) at this FS setting, so the raw
-reading of -1982 corresponds to 1982 * 0.160 = 317.1 mgauss =
-0.3171 gauss.
-
-In the LSM303DLHC, LSM303DLM, and LSM303DLH, the acceleration data
-registers actually contain a left-aligned 12-bit number, so the lowest
-4 bits are always 0, and the values should be shifted right by 4 bits
-(divided by 16) to be consistent with the conversion factors specified
-in the datasheets.
-
-Example: An LSM303DLH gives an accelerometer Z axis reading of -16144
-with its default full scale setting of +/- 2 g. Dropping the lowest 4
-bits gives a 12-bit raw value of -1009. The LA_So specification in the
-LSM303DLH datasheet (page 11) states a conversion factor of 1 mg/digit
-at this FS setting, so the value of -1009 corresponds to -1009 * 1 =
-1009 mg = 1.009 g.
-*/
-
+ * COLLECTING AND SENDING THE CALIBRATED MAGNETOMETER DATA VIA BLUETOOTH
+ * 
+ * code for ONE sensor
+ * for obtaining hard and soft iron values, run the "LSMCalibradetMag_RF.ino" script
+ * 
+ */
 #include <Wire.h>
 #include <LSM303.h>
 #include <RFduinoBLE.h>
 
 LSM303 compass;
 float magX, magY, magZ, rawX, rawY, rawZ;
+//float hardBias[3] = {0, 0, 0};
+//float softBias[3] = {1, 1, 1};
 //float hardBias[3] = {228.96, -50.77,  183.70};
 //float softBias[3] = {1.03, 0.98,  0.99};
-float hardBias[3] = {134.84, -34.25,  306.56};
-float softBias[3] = {1.08, 0.97,  0.96};
-//float hardBias[3] = {34.97, -8.86, 249.56};
-//float softBias[3] = {1.20, 0.94,  0.90};
+//float hardBias[3] = {134.84, -34.25,  306.56};
+//float softBias[3] = {1.08, 0.97,  0.96};
+float hardBias[3] = {97.96, 23.23, 260.82};
+float softBias[3] = {1.01, 0.99, 1.01};
 
-float conversionFactor = 0.479;   //for range +-12gauss
+float conversionFactorMag = 0.479;   //for range +-12gauss
+float conversionFactorAcc = 0.061;  //for range +-2g
 uint8_t buffer[14];
 char data[16];
 float fData[4];
@@ -68,6 +50,14 @@ void loop()
    Serial.print(magY); Serial.print("\t");
    Serial.println(magZ);
 
+  // accelerometer data
+   compass.readAcc();
+   
+   /*Serial.print("Acc\t");
+   Serial.print(compass.a.x*conversionFactorAcc); Serial.print("\t");
+   Serial.print(compass.a.y*conversionFactorAcc); Serial.print("\t");
+   Serial.println(compass.a.z*conversionFactorAcc);*/
+
    fData[0] = 0.0; 
    fData[1] = magX;
    fData[2] = magY;
@@ -76,8 +66,10 @@ void loop()
    for(int i=0; i<4; i++){
     memcpy(&data[i*sizeof(float)], &fData[i], sizeof(float));
    }
-  
+   unsigned long sec;
+   sec = millis();
    RFduinoBLE.send(data, 16);
+   Serial.println(millis()-sec);
    //delay(100);
 }
 
@@ -95,9 +87,9 @@ void getRawMagnet(){
    
   compass.readMag();
 
-  rawX = compass.m.x * conversionFactor;
-  rawY = compass.m.y * conversionFactor;
-  rawZ = compass.m.z * conversionFactor;
+  rawX = compass.m.x * conversionFactorMag;
+  rawY = compass.m.y * conversionFactorMag;
+  rawZ = compass.m.z * conversionFactorMag;
 }
 
 
