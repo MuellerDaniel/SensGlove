96fe4439df5b061c6c31db45eb0fa7444bd446be
MuellerDaniel <mueller.daniel89@gmx.de>
2016-01-24 13:40:15 +0100

    updated model calling

---
+++ python/handDim.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+''' static values for the dimensions on the hand '''
+
+# lengths of the phalanges
+#           prox-,   int-,     dist-    phalanges
+phalInd = np.array([0.03080, 0.02581, 0.01678])
+phalMid = np.array([0.03593, 0.03137, 0.01684])
+phalRin = np.array([0.03404, 0.02589, 0.01820])
+phalPin = np.array([0.02892, 0.02493, 0.01601])
+
+''' the origin of the coordinate systems is the joint of the index finger '''
+
+# sensor positions radial 
+#     [lateral(z(sensor_x)), radial[x_ulnar(sensor_-y),x_dorsal(sensor_+z)]]
+sInd_rad = [0.03,            [0,                0.024]]     # rack1
+sMid_rad = [0.03,            [0.02,             0.024]]
+sRin_rad = [0.03,            [0.04,             0.024]]
+sPin_rad = [0.03,            [0.06,             0.024]]
+
+# sensor positions Cartesian (aligned like sensor)
+sInd_car = np.array([-0.03, -0.0, 0.024] )    # rack1
+sMid_car = np.array([-0.03, -0.02, 0.024])
+sRin_car = np.array([-0.03, -0.04, 0.024])
+sPin_car = np.array([-0.03, -0.06, 0.024])
+#sInd_car = [-0.03, -0.0, 0.024]     # rack2
+#sMid_car = [-0.05, -0.022, 0.024]
+#sRin_car = [-0.03, -0.044, 0.024]
+#sPin_car = [-0.05, -0.066, 0.024]
+
+
+
+# joint positions radial
+#        [lateral(z), [x_ulnar(sensor_-y),x_dorsal(sensor_+z)]]
+jointInd_rad = [0.0,             [0,                    0.0]]
+jointMid_rad = [0.0,             [0.02,                    0.0]]
+jointRin_rad = [0.0,             [0.04,                    0.0]]
+jointPin_rad = [0.0,             [0.06,                    0.0]]
+
+# joint positions Cartesian 
+jointInd_car = np.array([0.0, 0.0, -0.0])
+jointMid_car = np.array([0.0, -0.02, 0.0])
+jointRin_car = np.array([0.0, -0.04, 0.0])
+jointPin_car = np.array([0.0, -0.06, -0.0])
--- python/perfectBangles.py
+++ python/perfectBangles.py
@@ -1,120 +1,115 @@
-import modelEqMultiCython as modE
-import dataAcquisitionMulti as datAc
+import numpy as np
+import modelCyl as modC
+import modelDip as modD
+import handDim as h
 import plotting as plo
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import *
-import time, random
-import fcnCyPy as fcn
+import time
 
-## absolute positions of wooden-joints
-#jointInd = [0.09138, 0.02957, -0.01087]         # to wooden-joint(index)
-#jointMid = [0.09138, 0.00920, -0.01087]          # to wooden-joint(middle)
-#jointRin = [0.09138, -0.01117, -0.01087]         # to wooden-joint(ring)
-#jointPin = [0.09138, -0.03154, -0.01087]         # to wooden-joint(pinky)
-#
-#
-## position of sensor
-#s1 = [0.06755, 0.02957, 0.]     # sensor beneath index   
-#s2 = [0.04755, 0.00920, 0.]     # sensor beneath middle
-#s3 = [0.06755, -0.01117, 0.]    # sensor beneath ring
-#s4 = [0.04755, -0.03012, 0.]    # sensor beneath pinky
+''' simulating and estimating with the models '''
 
-''' taking s0 as initial position... '''
-# position of sensor
-zSensor = 0.0
-#s1 = [0., 0.0, zSensor]
-#s2 = [s1[0]-0.02, -0.02063, zSensor]     # sensor beneath middle
-#s3 = [s1[0], -0.02063*2, zSensor]    # sensor beneath ring
-#s4 = [s1[0]-0.02, -0.02063*3, zSensor]    # sensor beneath pinky
-s1 = [0.0, 0.0, zSensor]
-s2 = [s1[0], -0.02063, zSensor]     # sensor beneath middle
-s3 = [s1[0], -0.02063*2, zSensor]    # sensor beneath ring
-s4 = [s1[0], -0.02063*3, zSensor]    # sensor beneath pinky
+sensList_cyl = [h.sInd_rad, h.sMid_rad, h.sRin_rad, h.sPin_rad]
+#sensList_cyl = [h.sPin_rad]
+fingList_cyl = [h.phalInd, h.phalMid, h.phalRin, h.phalPin]
+#fingList_cyl = [h.phalPin]
+jointList_cyl = [h.jointInd_rad, h.jointMid_rad, h.jointRin_rad, h.jointPin_rad]
+#jointList_cyl = [h.jointPin_rad]
 
-# position of wooden joints
-# TODO adjust them, when you have your rack!
-xJoint = 0.03       # distance between sensor rack and joints
-#zJoint = -0.02
-zJoint = -0.001
-jointInd = [xJoint, 0., -0.03006+zJoint]                                 # to wooden-joint(index)
-jointMid = [xJoint, jointInd[1]-0.01930, -0.02000+zJoint]          # to wooden-joint(middle)
-jointRin = [xJoint, jointMid[1]-0.01930, -0.02000+zJoint]         # to wooden-joint(ring)
-jointPin = [xJoint, jointRin[1]-0.01896, -0.02802+zJoint]         # to wooden-joint(pinky)
 
-# lengths of phalanges
-phalInd = [0.03080, 0.02581, 0.01678]
-phalMid = [0.03593, 0.03137, 0.01684]
-phalRin = [0.03404, 0.02589, 0.01820]
-phalPin = [0.02892, 0.02493, 0.01601]
+sensList_dip = [h.sInd_car, h.sMid_car, h.sRin_car, h.sPin_car]
+fingList_dip = [h.phalInd, h.phalMid, h.phalRin, h.phalPin]
+#fingList_dip = [h.phalPin]
+jointList_dip = [h.jointInd_car, h.jointMid_car, h.jointRin_car, h.jointPin_car]
+#jointList_dip = [h.jointPin_car]
 
-t = np.arange(0,1/2.*np.pi,0.01)
-angles = np.array([[0.,0.,0.]])
-#orienN = np.array([[0.,0.,0.]])
+
+''' simulation '''
+t = np.arange(0,np.pi/2,0.01)       # describing the angles
+angles_cyl = np.zeros((len(t),2*len(fingList_cyl)))
+angles_dip = np.zeros((len(t), 3*len(fingList_dip)))
 cnt = 0
 for i in t:
-    angles = np.append(angles,[[i,0,0]],axis=0)
-#    angles = np.append(angles,[[i, 
-#                                random.uniform(1,np.pi/2), 
-#                                random.uniform(0,0.5)]], axis=0)
+#    TODO adjust it on the number of fingers you want to measure
+    angles_cyl[cnt] = np.array([i, 0,    # angle index
+                              0, i,    # angle mid
+                              i, 0.4,    # angle rin
+                              i, i*0.3])   # angle pin
+    
+    angles_dip[cnt] = np.array([i, 0, 0,
+                                0, i, 0,
+                                i, 0.4, 0,
+                                i, i*0.3, 0])    
+    
     cnt += 1
-angles = angles[1:]    
-                
+    
+b_cyl = np.zeros((len(t), 2*len(sensList_cyl)))
+b_dip = np.zeros((len(t), 3*len(sensList_dip)))    
 
-''' calculating the B-field '''
-calcBInd = np.array([[0.,0.,0.]])
-calcBMid = np.array([[0.,0.,0.]])
-calcBRin = np.array([[0.,0.,0.]])
-calcBPin = np.array([[0.,0.,0.]])
-
-# for moving all 4 fingers the same way...
-for i in angles:
-    # the cummulative B-fields
-    calcBInd = np.append(calcBInd,modE.angToB(i,phalInd,jointInd,s1)+
-                                  modE.angToB(i,phalMid,jointMid,s1)+
-                                  modE.angToB(i,phalRin,jointRin,s1)+
-                                  modE.angToB(i,phalPin,jointPin,s1),axis=0)    
-    calcBMid = np.append(calcBMid,modE.angToB(i,phalInd,jointInd,s2)+
-                                  modE.angToB(i,phalMid,jointMid,s2)+
-                                  modE.angToB(i,phalRin,jointRin,s2)+
-                                  modE.angToB(i,phalPin,jointPin,s2),axis=0)
-    calcBRin = np.append(calcBRin,modE.angToB(i,phalInd,jointInd,s3)+
-                                  modE.angToB(i,phalMid,jointMid,s3)+
-                                  modE.angToB(i,phalRin,jointRin,s3)+
-                                  modE.angToB(i,phalPin,jointPin,s3),axis=0)
-    calcBPin = np.append(calcBPin,modE.angToB(i,phalInd,jointInd,s4)+
-                                  modE.angToB(i,phalMid,jointMid,s4)+
-                                  modE.angToB(i,phalRin,jointRin,s4)+
-                                  modE.angToB(i,phalPin,jointPin,s4),axis=0)    
-    # single B-fields                              
-#    calcBInd = np.append(calcBInd,modE.angToB(i,phalInd,jointInd,s1),axis=0)    
-calcBInd = calcBInd[1:]    
-calcBMid = calcBMid[1:]    
-calcBRin = calcBRin[1:]    
-calcBPin = calcBPin[1:]  
+cnt = 0
+for i in range(len(t)):
+    b_cyl[i] = modC.cy.angToBm_cyl_cy(angles_cyl[i],fingList_cyl,sensList_cyl,jointList_cyl)    # simulating cylindrical model
+#    b_dip[i] = modD.angToBm(angles_dip[i],fingList_dip,sensList_dip,jointList_dip)              # simulating dipole model
+    b_dip[i] = modD.cy.angToBm_cy(angles_dip[i],fingList_dip,sensList_dip,jointList_dip)
+    
+plt.close('all')    
+#plo.plotter2d((b_cyl,b_dip),("cyl","dip"),shareAxis=False)
+plo.plotter2d((b_dip[:,:3], b_dip[:,3:6], b_dip[:,6:9], b_dip[:,9:]),("a","a","a","a"))
+plo.plotter2d((b_cyl[:,:2], b_cyl[:,2:4], b_cyl[:,4:6], b_cyl[:,6:]),("c","c","c","c"))    
 
 
-''' save it to a file in the desired format '''
-#fi = open("151103_perfectB_straight",'w')
-#for i in range(len(calcBInd)):
-#    fi.write(str(0) + "\t" + str(calcBInd[i][0]) + "\t" + 
-#                            str(calcBInd[i][1]) + "\t" + 
-#                            str(calcBInd[i][2]) + "\n")
-#    fi.write(str(1) + "\t" + str(calcBMid[i][0]) + "\t" + 
-#                            str(calcBMid[i][1]) + "\t" + 
-#                            str(calcBMid[i][2]) + "\n")
-#    fi.write(str(2) + "\t" + str(calcBRin[i][0]) + "\t" + 
-#                            str(calcBRin[i][1]) + "\t" + 
-#                            str(calcBRin[i][2]) + "\n")
-#    fi.write(str(3) + "\t" + str(calcBPin[i][0]) + "\t" + 
-#                            str(calcBPin[i][1]) + "\t" + 
-#                            str(calcBPin[i][2]) + "\n")
-#fi.close()                            
+''' estimation '''
+## cylindrical model
+estAng_cyl = np.zeros((len(b_cyl), 2*len(fingList_cyl)))
+bnds_cyl = ((0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.))
 
-plo.plotter2d((calcBInd, calcBMid, calcBRin, calcBPin),("ind","mid","rin","pin"),shareAxis=False)
-#plt.show()
-# the angles...
-#plt.figure()
-#plt.plot(angles[:,0],linestyle='-')
-#plt.plot(angles[:,1],linestyle='-.')
-#plt.plot(angles[:,2],linestyle=':')
+startCyl = time.time()
+cnt = 0
+for i in b_cyl[1:]:
+    print "cylindrical estimation step: ", cnt
+    tmp = modC.estimateAng_cyl(estAng_cyl[cnt], fingList_cyl, sensList_cyl, jointList_cyl, i, bnds=bnds_cyl)
+    print "function value: ", tmp.fun
+    estAng_cyl[cnt+1] = tmp.x
+    
+    cnt += 1
+
+timeCyl = time.time()-startCyl    
+plo.plotter2d((estAng_cyl[:,:2], estAng_cyl[:,2:4],estAng_cyl[:,4:6],estAng_cyl[:,6:]),("i","m","r","p"))    
+    
+
+## dipole model
+estAng_dip = np.zeros((len(b_dip), 3*len(fingList_dip)))    
+bnds_dip = ((0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.),
+            (0.0,np.pi/2.))
+
+startDip = time.time()
+cnt = 0
+for i in b_dip[1:]:
+    print "dipole estimation step: ", cnt
+#    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i)
+    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i,bnds=bnds_dip)
+    print "function value: ", tmp.fun
+    estAng_dip[cnt+1] = tmp.x
+    
+    cnt += 1
+
+timeDip = time.time()-startDip
+plo.plotter2d((estAng_dip[:,:2], estAng_dip[:,2:4],estAng_dip[:,4:6],estAng_dip[:,6:]),("di","dm","dr","dp"))    
+print "time for cyl: ", timeCyl
+print "time for dip: ", timeDip
