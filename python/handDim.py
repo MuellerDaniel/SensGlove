9faa782f75a4a2f320aadbed6b702b15af3d450d
MuellerDaniel <mueller.daniel89@gmx.de>
2016-01-26 16:15:03 +0100

    script containing the hand dimensions

--- python/handDim.py
+++ python/handDim.py
@@ -11,14 +11,8 @@ phalPin = np.array([0.02892, 0.02493, 0.01601])
 
 ''' the origin of the coordinate systems is the joint of the index finger '''
 
-# sensor positions radial 
-#     [lateral(z(sensor_x)), radial[x_ulnar(sensor_-y),x_dorsal(sensor_+z)]]
-sInd_rad = [0.03,            [0,                0.024]]     # rack1
-sMid_rad = [0.03,            [0.02,             0.024]]
-sRin_rad = [0.03,            [0.04,             0.024]]
-sPin_rad = [0.03,            [0.06,             0.024]]
-
 # sensor positions Cartesian (aligned like sensor)
+# for dipole approach
 sInd_car = np.array([-0.03, -0.0, 0.024] )    # rack1
 sMid_car = np.array([-0.03, -0.02, 0.024])
 sRin_car = np.array([-0.03, -0.04, 0.024])
@@ -28,17 +22,19 @@ sPin_car = np.array([-0.03, -0.06, 0.024])
 #sRin_car = [-0.03, -0.044, 0.024]
 #sPin_car = [-0.05, -0.066, 0.024]
 
+# for cylindrical approach
+sInd_carC = np.array([0.03, -0.0, -0.024] )    # rack1
+sMid_carC = np.array([0.03, -0.02, -0.024])
+sRin_carC = np.array([0.03, -0.04, -0.024])
+sPin_carC = np.array([0.03, -0.06, -0.024])
 
-
-# joint positions radial
-#        [lateral(z), [x_ulnar(sensor_-y),x_dorsal(sensor_+z)]]
-jointInd_rad = [0.0,             [0,                    0.0]]
-jointMid_rad = [0.0,             [0.02,                    0.0]]
-jointRin_rad = [0.0,             [0.04,                    0.0]]
-jointPin_rad = [0.0,             [0.06,                    0.0]]
-
-# joint positions Cartesian 
+# joint positions Cartesian
 jointInd_car = np.array([0.0, 0.0, -0.0])
 jointMid_car = np.array([0.0, -0.02, 0.0])
 jointRin_car = np.array([0.0, -0.04, 0.0])
 jointPin_car = np.array([0.0, -0.06, -0.0])
+
+# jointInd_car = np.array([0.0, 0.0, -0.0])
+# jointMid_car = np.array([0.0, -0.02, 0.0])
+# jointRin_car = np.array([0.0, -0.04, 0.0])
+# jointPin_car = np.array([0.0, -0.06, -0.0])
