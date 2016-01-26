11a0b32cdcd1ad00850762917fa6e9ebd09c3d95
MuellerDaniel <mueller.daniel89@gmx.de>
2016-01-26 16:16:50 +0100

    adjusted for 3D cyl model

--- python/modelCyl.py
+++ python/modelCyl.py
@@ -1,230 +1,6 @@
 import numpy as np
 from scipy.optimize import *
-from sympy import *
-import time
 import cylModel as cy
-
-
-def cel(kc,p,c,s):
-    ''' approximate a complete elliptical integral '''    
-#    print "kc ", kc       
-#    print "p ", p
-#    print "c ", c
-#    print "s ", s
-       
-    if kc == 0:
-        c = None    # or nan???    
-        return c
-    
-    # errtol = 1.0e-6;
-    errtol = 1.0e-6
-    k = abs(kc)
-    pp = p
-    cc = c
-    ss = s
-    em = 1.0
-    
-    if p > 0:
-        pp = sqrt(p)
-        ss = s/pp
-    else:
-        f = kc*kc
-        q = 1.0-f
-        g = 1.0-pp
-        f = f-pp
-        q = q*(ss-c*pp)
-        pp = sqrt(f/g)
-        cc = (c-ss)/g
-        ss = q/((g*g)*pp)+cc*pp
-    
-    f = cc
-    cc = cc+ss/pp
-    g = k/pp
-    ss = 2*(ss+f*g)
-    pp = g+pp
-    g = em
-    em = k+em
-    kk = k
-    
-#    print "before while\n", abs(g-k), g*errtol
-    cnt = 0
-    while (abs(g-k) > g*errtol):
-       k = 2*sqrt(kk)
-       kk = k*em
-       f = cc
-       cc = cc+ss/pp
-       g = kk/pp
-       ss = 2*(ss+f*g)
-       pp = g+pp
-       g = em
-       em = k+em    
-       
-       cnt += 1
-       
-#       print "in while\n", g, k, g*errtol
-       
-#       if abs(g-k) > 10:
-#           print "TOO BIG"
-#           return 0
-    
-    c = (pi/2.0)*(ss+cc*em)/(em*(em+pp))
-#    print "steps needed: ", cnt
-    return c
-    
-
-def R_F(x,y,z):
-    ''' Compute Carlson's elliptic integral of first kind '''
-    print "in R_F"
-    
-    # some constants...
-#    errtol = 0.0025
-    errtol = 0.1
-    C1 = 1.0/24.0
-    C2 = 0.1
-    C3 = 3.0/44.0
-    C4 = 1.0/14.0
-    
-    # TODO checks on x,y,z are missing...
-    
-    xt = x
-    yt = y
-    zt = z    
-    
-    while True:
-        sqrtx = sqrt(xt)
-        sqrty = sqrt(yt)
-        sqrtz = sqrt(zt)
-        alamb = sqrtx*(sqrty+sqrtz)+sqrty*sqrtz
-        xt = 0.25*(xt+alamb)
-        yt = 0.25*(yt+alamb)
-        zt = 0.25*(zt+alamb)
-        ave = (xt+yt+zt)/3.
-        delx = (ave-xt)/ave
-        dely = (ave-yt)/ave
-        delz = (ave-zt)/ave
-        if max(max(abs(delx),abs(dely)),abs(delz)) > errtol:
-            break
-        
-    e2 = delx*dely-delz*delz
-    e3 = delx*dely*delz
-    return (1.0+(C1*e2-C2-C3*e3)*e2+C4*e3)/sqrt(ave)
-    
-    
-def R_C(x,y):
-    ''' compute Carlson's elliptic integral of second kind '''
-    print "in R_C"
-    
-#    errtol = 0.0012 
-    errtol = 0.1
-    C1 = 0.3
-    C2 = 1.0/7.0
-    C3 = 0.375
-    C4 = 9.0/22.0
-    
-    # TODO checks on variables...    
-    
-    if y > 0.0:
-        xt = x
-        yt = y
-        w = 1.0
-    else:
-        xt = x-y
-        yt = -y
-        w = sqrt(x)/sqrt(xt)
-    
-    while True:
-        alamb = 2.0*sqrt(xt)*sqrt(yt)+yt
-        xt = 0.25*(xt+alamb)
-        yt = 0.25*(yt+alamb)
-        ave = (xt+yt+yt)/3.
-        s = (yt-ave)/ave
-#        print ave
-        if abs(s) > errtol:
-            break
-
-    return w*(1.0+s*s*(C1+s*(C2+s*(C3+s*C4))))/sqrt(ave);
-
-    
-
-def R_J(x,y,z,p):
-    ''' compute Carlson's elliptic integral of third kind '''
-    print "in R_J"
-    
-    # some constants...
-#    errtol = 0.0015   
-    errtol = 0.1
-    C1=3.0/14.0 
-    C2=1.0/3.0
-    C3=3.0/22.0
-    C4=3.0/26.0 
-    C5=0.75*C3 
-    C6=1.5*C4 
-    C7=0.5*C2 
-    C8=C3+C3
-    
-    # TODO checks on input variables are missing...   
-    
-    sum=0.0
-    fac=1.0
-    if p > 0.0: 
-        xt = x
-        yt = y
-        zt = z
-        pt = p
-    else:
-        xt = min(min(x,y),z)
-        zt = max(max(x,y),z)
-        yt = x+y+z-xt-zt
-        a = 1.0/(yt-p)
-        b = a*(zt-yt)*(yt-xt)
-        pt = yt+b
-        rho = xt*zt/yt
-        tau = p*pt/yt
-        rcx = R_C(rho,tau) 
-        
-    while True:
-        sqrtx=sqrt(xt)
-        sqrty=sqrt(yt)
-        sqrtz=sqrt(zt)
-        alamb=sqrtx*(sqrty+sqrtz)+sqrty*sqrtz
-        alpha=(pt*(sqrtx+sqrty+sqrtz)+sqrtx*sqrty*sqrtz)**2
-        beta=pt*((pt+alamb)**2)
-        sum += fac*R_C(alpha,beta)
-        fac=0.25*fac
-        xt=0.25*(xt+alamb)
-        yt=0.25*(yt+alamb)
-        zt=0.25*(zt+alamb)
-        pt=0.25*(pt+alamb)
-        ave=0.2*(xt+yt+zt+pt+pt)
-        delx=(ave-xt)/ave
-        dely=(ave-yt)/ave
-        delz=(ave-zt)/ave
-        delp=(ave-pt)/ave                
-        if max(max(abs(delx),abs(dely)),max(abs(delz),abs(delp))) > errtol:
-            break
-
-    ea=delx*(dely+delz)+dely*delz
-    eb=delx*dely*delz
-    ec=delp*delp
-    ed=ea-3.0*ec
-    ee=eb+2.0*delp*(ea-ec)
-    ans=3.0*sum+fac*(1.0+ed*(-C1+C5*ed-C6*ee)+eb*(C7+delp*(-C8+delp*C4))
-    		+delp*ea*(C2-delp*C3)-C2*delp*ec)/(ave*sqrt(ave))
-
-    if p <= 0.0: 
-        ans=a*(b*ans+3.0*(rcx-R_F(xt,yt,zt)))
-    
-    return ans
-    
-    
-def cel_car(kc,p,c,s):
-    ''' approximate a complete elliptical integral with Carlson's functions '''    
-    print "cel_car"
-    
-    res = c*R_F(0.,kc**2,1.)+((s-p*c)*R_J(0.,kc**2,1.,p))/3.
-    
-    return res
-
 
 def cel_bul(kc,p,c,s):
     ''' approximate a complete elliptical integral with Bulirsch algorithm '''    
@@ -238,14 +14,14 @@ def cel_bul(kc,p,c,s):
     em = 1.0
     
     if p > 0:
-        p = sqrt(p)
+        p = np.sqrt(p)
         s /= p
     else:
         f = kc**2
         q = (1-f)*(s-c*p)
         g = 1-p
         f -= p
-        p = sqrt(f/g)
+        p = np.sqrt(f/g)
         c = (c-s)/g
         s = -q/((g**2)*p)+c*p
     
@@ -259,7 +35,7 @@ def cel_bul(kc,p,c,s):
         
     cnt = 0        
     while (abs(g-k) > (g*errtol)):
-       k = 2*sqrt(kk)
+       k = 2*np.sqrt(kk)
        kk = k*em
        f = c
        c += (s/p)
@@ -270,7 +46,7 @@ def cel_bul(kc,p,c,s):
        
        cnt += 1
            
-    c = (pi/2)*(s+c*em)/(em*(em+p))
+    c = (np.pi/2)*(s+c*em)/(em*(em+p))
 #    c = float(c)
 #    print "steps needed: ", cnt
     return c    
@@ -292,47 +68,54 @@ def calcB_cyl(pos, ang):
         the magnetic field [B_z, B_rho]
     '''
              
-
-    rotMatPos = np.array([[cos(ang), -sin(ang)],
-                          [sin(ang), cos(ang)]])
-    cylCo = np.dot(pos,rotMatPos)    # rotated cylindrical coordinates             
-        
-    z = cylCo[0]
-    rho = cylCo[1]
+    rotMatPos = np.array([[np.cos(ang) , 0, np.sin(ang)],
+                          [0           , 1, 0       ],
+                          [-np.sin(ang), 0, np.cos(ang)]])
+                          
+    posRot = np.dot(pos,rotMatPos)
+    
+    z = posRot[0]
+    rho = np.sqrt(posRot[1]**2+posRot[2]**2)
+    
+    phi = np.arctan2(posRot[1], posRot[2])  # perhaps +np.pi or *-1, because z is neg???
+    
+#    print "phi: ",phi
     
     a = 0.0025     # radius [m]
     b = 0.015/2    # half length of magnet [m]
     # magic value...
-    Bo = 1.0e+3*4.0107      # magnetic constant
+    Bo = 1.0e+3*4.0107      # magnetic constant    
     
     # component calculations
     z_pos = z+b
     z_neg = z-b
     
-    alpha_pos = a/sqrt(z_pos**2+(rho+a)**2)
-    alpha_neg = a/sqrt(z_neg**2+(rho+a)**2)
+    alpha_pos = a/np.sqrt(z_pos**2+(rho+a)**2)
+    alpha_neg = a/np.sqrt(z_neg**2+(rho+a)**2)
     
-    beta_pos = z_pos/sqrt(z_pos**2+(rho+a)**2)
-    beta_neg = z_neg/sqrt(z_neg**2+(rho+a)**2)
+    beta_pos = z_pos/np.sqrt(z_pos**2+(rho+a)**2)
+    beta_neg = z_neg/np.sqrt(z_neg**2+(rho+a)**2)
     
     gamma = (a-rho)/(a+rho)
     
-    k_pos = sqrt((z_pos**2+(a-rho)**2)/(z_pos**2+(a+rho)**2))
-    k_neg = sqrt((z_neg**2+(a-rho)**2)/(z_neg**2+(a+rho)**2))
+    k_pos = np.sqrt((z_pos**2+(a-rho)**2)/(z_pos**2+(a+rho)**2))
+    k_neg = np.sqrt((z_neg**2+(a-rho)**2)/(z_neg**2+(a+rho)**2))
 
     # cel calculation with Bulirsch's algorithm    
-#    B_rho = Bo*(alpha_pos*cel_bul(k_pos,1.,1.,-1.)-alpha_neg*cel_bul(k_neg,1.,1.,-1.))
-#    B_z   = (Bo*a)/(a+rho)*(beta_pos*cel_bul(k_pos,gamma**2,1.,gamma)-beta_neg*cel_bul(k_neg,gamma**2,1.,gamma))
-    B_rho = Bo*(alpha_pos*cy.cel_bul_cy(k_pos,1.,1.,-1.)-alpha_neg*cy.cel_bul_cy(k_neg,1.,1.,-1.))
-    B_z   = (Bo*a)/(a+rho)*(beta_pos*cy.cel_bul_cy(k_pos,gamma**2,1.,gamma)-beta_neg*cy.cel_bul_cy(k_neg,gamma**2,1.,gamma))
-        
-    B = np.dot(np.array([B_z, B_rho]),np.linalg.inv(rotMatPos))  
-#    B[1] *= -1
-#    return (B, cylCo)
-#    B = B.astype('float')
+    B_rho = Bo*(alpha_pos*cel_bul(k_pos,1.,1.,-1.)-alpha_neg*cel_bul(k_neg,1.,1.,-1.))
+    B_lat   = (Bo*a)/(a+rho)*(beta_pos*cel_bul(k_pos,gamma**2,1.,gamma)-beta_neg*cel_bul(k_neg,gamma**2,1.,gamma))
+            
+
+    ''' 3D!!! '''            
+    
+    B = np.array([B_lat, B_rho*np.sin(phi), B_rho*np.cos(phi)])
+#    B = np.array([B_lat, B_rho*np.cos(phi), B_rho*np.sin(phi)])
+    B = np.dot(B,np.linalg.inv(rotMatPos))
+            
     return B
     
-
+    
+    
 def angToP_cyl(angles, finger):
     ''' calculate the 2d(cylindrical) position according to joint angles and fingerlengths
     
@@ -355,21 +138,14 @@ def angToP_cyl(angles, finger):
     pos = np.array([(1*(finger_0*np.sin(np.pi/2.) + finger[0]*np.sin(np.pi/2-theta[0]) +              # x
                 finger[1]*np.sin(np.pi/2.-theta[0]-theta[1]) +
                 finger[2]*np.sin(np.pi/2.-theta[0]-theta[1]-theta[2]))),
-
+                0,
                 (-1*(finger[0]*np.cos(np.pi/2.-theta[0]) +               # z (*-1 because you move in neg. z-direction)
                 finger[1]*np.cos(np.pi/2.-theta[0]-theta[1]) +
                 finger[2]*np.cos(np.pi/2.-theta[0]-theta[1]-theta[2]))*np.cos(theta_k))])
                 
-    return pos                
+    return pos                    
     
-
-def diffRadial(p1,p2):
-    tmp = np.sqrt((p1[1][0]-p2[1][0])**2+(p1[1][1]-p2[1][1])**2)*-1
-    res = np.array([p1[0]-p2[0],tmp])    
-#    print "diffRadial:\n", res
-    return res 
-
-
+    
 def angToB_cyl(angles, fingerL, sPos, jointPos):
     ''' calculate the B-field for given finger angels. CYLINDRICAL MODEL!!!
     
@@ -379,45 +155,46 @@ def angToB_cyl(angles, fingerL, sPos, jointPos):
         angle of the joints [angle_MCP, angle_PIP]
     fingerL : 3darray
         length of the finger phalanges [length_proximal, length_intermediate, length_distal]
-    sPos : 2darray
+    sPos : 3darray
         the sensor position, relative to the joint ([x_lateral, x_radial])
-    jointPos : 2darray
+    jointPos : 3darray
         position of the finger joint, relative to the index joint ([x_lateral, x_radial])
         
     Returns
     -------
-    B : 2darray
-        the calculated B-field at the sensor [B_z, B_rho]
-    '''
-    
-    if type(sPos[1]) == type(jointPos[1]) == list:
-        p = cy.angToP_cyl_cy(angles,fingerL)+diffRadial(sPos,jointPos)
-#        print "p\n",p
+    B : 3darray
+        the calculated B-field at the sensor 
+    '''    
+    # pass everything as lists!!!!
+    if type(sPos) == type(jointPos) == type(fingerL) == list:
+#        print "here!"
+        if len(jointPos) == len(fingerL) == len(angles)/2:
+            B = np.zeros((3*len(sPos),))
+            sCnt = 0
+            for i in sPos:
+                for j in range(len(jointPos)):
+                    actAngles = angles[j*2:j*2+2]
+                    p = angToP_cyl(actAngles, fingerL[j])+(i-jointPos[j])                         
+                    ang = sum(actAngles)+(2./3.*actAngles[1])                    
+                    B[sCnt*3:sCnt*3+3] += calcB_cyl(p,ang)
+                sCnt += 1
+            return B                
+        else:
+            print "ERROR! wrong combination of lists"
+            return -1                
+
+    # or as a np.ndarray...            
     else:
-        p = cy.angToP_cyl_cy(angles,fingerL)+sPos-jointPos
-#        print "else p\n",p
-#    print "diff old: ", np.array([0.,0.])+jointPos-sPos
-#    p = cy.angToP_cyl_cy(angles,fingerL)+diffRadial(sPos,jointPos)
-#    p = angToP_cyl(angles,fingerL)+diffRadial(sPos,jointPos)
-#    print "diff new: ",diffRadial(sPos,jointPos)
-
-    ang = sum(angles)+(2./3.*angles[1])
-    ang *= -1        
-    
-#    B = calcB_cyl(p,ang)    
-    B = cy.calcB_cyl_cy(p,ang)
-    B[1] *= 1      # really?
-#    B = B.astype('float')
-#    return (B, p, ang)
-    return B    
-
-''' estimation of joint angles '''
-
+        p = angToP_cyl(angles, fingerL)+(sPos-jointPos)    
+        ang = sum(angles)+(2./3.*angles[1])
+        B = calcB_cyl(p,ang)    
+        return B   
+        
+        
 def minimizeAng_cyl(ang, fingerL, sPos, jointPos, measB):
     ''' objective function to minimize... '''
     
     dif = measB - angToB_cyl(ang, fingerL, sPos, jointPos)
-#    dif = measB - cy.angToB_cyl_cy(ang, fingerL, sPos, jointPos)
     
     dif = dif.astype('float')    
     res = np.linalg.norm(dif)
@@ -425,7 +202,7 @@ def minimizeAng_cyl(ang, fingerL, sPos, jointPos, measB):
     return res
     
     
-def estimateAng_cyl(ang_0, fingerL, sPos, jointPos, measB):
+def estimateAng_cyl(ang_0, fingerL, sPos, jointPos, measB, bnds=None):
     ''' estimating the joint angles
     
     Parameters
@@ -446,18 +223,14 @@ def estimateAng_cyl(ang_0, fingerL, sPos, jointPos, measB):
     
     '''
     
-    res = minimize(minimizeAng_cyl,ang_0,args=(fingerL, sPos, jointPos, measB),method='bfgs', tol=1.e-05)
-#    res = minimize(minimizeAng_cyl,ang_0,args=(fingerL,sPos,jointPos,measB),method='slsqp',bounds=bnds)    
-#    res = fmin_l_bfgs_b(minimizeAng_cyl, ang_0, args=(fingerL,sPos,jointPos,measB),bounds=bnds,approx_grad=1,pgtol=1e-10)
-#    res = fmin_l_bfgs_b(cy.minimizeAng_cyl_cy, ang_0, args=(fingerL,sPos,jointPos,measB),bounds=bnds,approx_grad=1,pgtol=1e-10)
+#    res = minimize(minimizeAng_cyl,ang_0,args=(fingerL, sPos, jointPos, measB),method='bfgs', tol=1.e-05)
+#    res = minimize(cy.minimizeAng_cyl,ang_0,args=(fingerL, sPos, jointPos, measB),method='bfgs', tol=1.e-05)
+    res = minimize(cy.minimizeAng_cyl,ang_0,args=(fingerL, sPos, jointPos, measB),method='slsqp', tol=1.e-05, bounds=bnds)
     
-#    res = minimize(cy.minimizeAng_cyl_cy,ang_0,args=(fingerL, sPos, jointPos, measB),method='bfgs', tol=1.e-05)    
-#    radDist = np.array(diffRadial(sPos,jointPos))
-#    res = minimize(cy.minimizeAng_cyl_cy,ang_0,args=(fingerL, radDist, measB),method='bfgs', tol=1.e-05)
     
-    return res
+    return res       
     
-
+    
 
 ''' estimation of position, given angle '''
 
@@ -470,7 +243,7 @@ def minimizeB_cyl(pos, ang, measB):
     
     return res
     
-def estimatePos_cyl(pos_0, ang, measB):
+def estimatePos_cyl(pos_0, ang, measB, bnds=None):
     ''' estimating the position
     
     Parameters
@@ -487,14 +260,15 @@ def estimatePos_cyl(pos_0, ang, measB):
     res : OptimizeResult
     
     '''
-    
-#    res = fmin_l_bfgs_b(minimizeB_cyl,pos_0,args=(ang,measB),approx_grad=1)
-#    res = minimize(minimizeB_cyl,pos_0,args=(ang,measB),method='slsqp')
+
     res = minimize(minimizeB_cyl,pos_0,args=(ang, measB),method='bfgs', tol=1.e-05)
+#    res = minimize(cy.minimizeB_cyl_cy,pos_0,args=(ang, measB),method='bfgs', tol=1.e-05)
+#    res = minimize(minimizeB_cyl,pos_0,args=(ang, measB),method='slsqp', tol=1.e-05, bounds=bnds)
     
     return res
     
 
-    
+
+  
     
     
--- python/perfectBangles.py
+++ python/perfectBangles.py
@@ -8,18 +8,18 @@ import time
 
 ''' simulating and estimating with the models '''
 
-sensList_cyl = [h.sInd_rad, h.sMid_rad, h.sRin_rad, h.sPin_rad]
+sensList_cyl = [h.sInd_carC, h.sMid_carC, h.sRin_carC, h.sPin_carC]
 #sensList_cyl = [h.sPin_rad]
-fingList_cyl = [h.phalInd, h.phalMid, h.phalRin, h.phalPin]
+fingList_cyl = [h.phalMid]
 #fingList_cyl = [h.phalPin]
-jointList_cyl = [h.jointInd_rad, h.jointMid_rad, h.jointRin_rad, h.jointPin_rad]
+jointList_cyl = [h.jointMid_car]
 #jointList_cyl = [h.jointPin_rad]
 
 
 sensList_dip = [h.sInd_car, h.sMid_car, h.sRin_car, h.sPin_car]
-fingList_dip = [h.phalInd, h.phalMid, h.phalRin, h.phalPin]
+fingList_dip = [h.phalMid]
 #fingList_dip = [h.phalPin]
-jointList_dip = [h.jointInd_car, h.jointMid_car, h.jointRin_car, h.jointPin_car]
+jointList_dip = [h.jointMid_car]
 #jointList_dip = [h.jointPin_car]
 
 
@@ -30,31 +30,32 @@ angles_dip = np.zeros((len(t), 3*len(fingList_dip)))
 cnt = 0
 for i in t:
 #    TODO adjust it on the number of fingers you want to measure
-    angles_cyl[cnt] = np.array([i, 0,    # angle index
-                              0, i,    # angle mid
-                              i, 0.4,    # angle rin
-                              i, i*0.3])   # angle pin
+    angles_cyl[cnt] = np.array([i, 0])#,    # angle index
+#                              0, i,    # angle mid
+#                              i, 0.4,    # angle rin
+#                              i, i*0.3])   # angle pin
     
-    angles_dip[cnt] = np.array([i, 0, 0,
-                                0, i, 0,
-                                i, 0.4, 0,
-                                i, i*0.3, 0])    
+    angles_dip[cnt] = np.array([i, 0, 0])#,
+#                                0, i, 0,
+#                                i, 0.4, 0,
+#                                i, i*0.3, 0])    
     
     cnt += 1
     
-b_cyl = np.zeros((len(t), 2*len(sensList_cyl)))
+b_cyl = np.zeros((len(t), 3*len(sensList_cyl)))
 b_dip = np.zeros((len(t), 3*len(sensList_dip)))    
 
 cnt = 0
 for i in range(len(t)):
-    b_cyl[i] = modC.cy.angToBm_cyl_cy(angles_cyl[i],fingList_cyl,sensList_cyl,jointList_cyl)    # simulating cylindrical model
+    b_cyl[i] = modC.cy.angToB_cyl(angles_cyl[i],fingList_cyl,sensList_cyl,jointList_cyl)    # simulating cylindrical model
 #    b_dip[i] = modD.angToBm(angles_dip[i],fingList_dip,sensList_dip,jointList_dip)              # simulating dipole model
-    b_dip[i] = modD.cy.angToBm_cy(angles_dip[i],fingList_dip,sensList_dip,jointList_dip)
+#    b_dip[i] = modD.cy.angToBm_cy(angles_dip[i],fingList_dip,sensList_dip,jointList_dip)
     
 plt.close('all')    
 #plo.plotter2d((b_cyl,b_dip),("cyl","dip"),shareAxis=False)
-plo.plotter2d((b_dip[:,:3], b_dip[:,3:6], b_dip[:,6:9], b_dip[:,9:]),("a","a","a","a"))
-plo.plotter2d((b_cyl[:,:2], b_cyl[:,2:4], b_cyl[:,4:6], b_cyl[:,6:]),("c","c","c","c"))    
+#plo.plotter2d((b_dip[:,:3], b_dip[:,3:6], b_dip[:,6:9], b_dip[:,9:]),("a","a","a","a"))
+#plo.plotter2d((b_cyl[:,:2], b_cyl[:,2:4], b_cyl[:,4:6], b_cyl[:,6:]),("c","c","c","c"))  
+plo.plotter2d((b_cyl,),("mid",))  
 
 
 ''' estimation '''
@@ -69,18 +70,18 @@ bnds_cyl = ((0.0,np.pi/2.),
             (0.0,np.pi/2.),
             (0.0,np.pi/2.))
 
-startCyl = time.time()
-cnt = 0
-for i in b_cyl[1:]:
-    print "cylindrical estimation step: ", cnt
-    tmp = modC.estimateAng_cyl(estAng_cyl[cnt], fingList_cyl, sensList_cyl, jointList_cyl, i, bnds=bnds_cyl)
-    print "function value: ", tmp.fun
-    estAng_cyl[cnt+1] = tmp.x
-    
-    cnt += 1
-
-timeCyl = time.time()-startCyl    
-plo.plotter2d((estAng_cyl[:,:2], estAng_cyl[:,2:4],estAng_cyl[:,4:6],estAng_cyl[:,6:]),("i","m","r","p"))    
+#startCyl = time.time()
+#cnt = 0
+#for i in b_cyl[1:]:
+#    print "cylindrical estimation step: ", cnt
+#    tmp = modC.estimateAng_cyl(estAng_cyl[cnt], fingList_cyl, sensList_cyl, jointList_cyl, i, bnds=bnds_cyl)
+#    print "function value: ", tmp.fun
+#    estAng_cyl[cnt+1] = tmp.x
+#    
+#    cnt += 1
+#
+#timeCyl = time.time()-startCyl    
+#plo.plotter2d((estAng_cyl[:,:2], estAng_cyl[:,2:4],estAng_cyl[:,4:6],estAng_cyl[:,6:]),("i","m","r","p"))    
     
 
 ## dipole model
@@ -98,18 +99,19 @@ bnds_dip = ((0.0,np.pi/2.),
             (0.0,np.pi/2.),
             (0.0,np.pi/2.))
 
-startDip = time.time()
-cnt = 0
-for i in b_dip[1:]:
-    print "dipole estimation step: ", cnt
-#    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i)
-    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i,bnds=bnds_dip)
-    print "function value: ", tmp.fun
-    estAng_dip[cnt+1] = tmp.x
-    
-    cnt += 1
+#startDip = time.time()
+#cnt = 0
+#for i in b_dip[1:]:
+#    print "dipole estimation step: ", cnt
+##    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i)
+#    tmp = modD.estimate_BtoAng(estAng_dip[cnt],fingList_dip,jointList_dip,sensList_dip,i,bnds=bnds_dip)
+#    print "function value: ", tmp.fun
+#    estAng_dip[cnt+1] = tmp.x
+#    
+#    cnt += 1
+#
+#timeDip = time.time()-startDip
 
-timeDip = time.time()-startDip
-plo.plotter2d((estAng_dip[:,:2], estAng_dip[:,2:4],estAng_dip[:,4:6],estAng_dip[:,6:]),("di","dm","dr","dp"))    
-print "time for cyl: ", timeCyl
-print "time for dip: ", timeDip
+#plo.plotter2d((estAng_dip[:,:2], estAng_dip[:,2:4],estAng_dip[:,4:6],estAng_dip[:,6:]),("di","dm","dr","dp"))    
+#print "time for cyl: ", timeCyl
+#print "time for dip: ", timeDip
